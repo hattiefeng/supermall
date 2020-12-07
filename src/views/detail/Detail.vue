c76e59149cbbb3b0e904d68ab4d1a8ee2df1058c
hattiefeng <iammaohaiyan@126.com>
2020-12-07 11:33:39 +0800

    详情页的轮播图

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :top-images="topImages"></detail-swiper>
   </div>
 </template>
 
 <script>
   import DetailNavBar from './childComps/DetailNavBar.vue';
+  import DetailSwiper from './childComps/DetailSwiper.vue'
 
   import {getGoodsDetail} from 'network/detail.js'
 
   export default {
+    name: "Detail",
     components: {
-      DetailNavBar
+      DetailNavBar,
+      DetailSwiper,
+      getGoodsDetail
     },
     data() {
       return {
@@ -19,24 +24,14 @@
         topImages: []
       }
     },
-    watch: {
-      '$route'(to, from) {
-        //监听路由是否变化
-        if (this.$route.params.iid) {
-          // 判断条件1  判断传递值的变化
-          this.iid = this.$route.params.iid;
-          //获取文章数据
-          getGoodsDetail(this.iid).then(res => {
-            console.log(res);
-            this.topImages = res.result.itemInfo.topImages;
-            console.log(this.topImages);
-          }).catch(err => {
-            console.log("数据请求失败");
-          })
-          
-        }
-      }
-
+    created() {
+      this.iid = this.$route.params.iid;
+      getGoodsDetail(this.iid).then(res => {
+        this.topImages = res.result.itemInfo.topImages;
+        // console.log(this.topImages);
+      }).catch(err => {
+        console.log("数据请求失败");
+      })
     }
 
   }
