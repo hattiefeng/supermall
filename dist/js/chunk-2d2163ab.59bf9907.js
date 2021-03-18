bf2d4e286a805eb1155efaa667bbb2e68c1d1f47
liuyk <271007729@qq.com>
2021-03-18 11:40:33 +0800

    项目完成打包

--- dist/js/chunk-2d2163ab.59bf9907.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2163ab"],{c228:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h2",[n._v("购物车")])},a=[],u={name:"cart"},l=u,r=t("2877"),s=Object(r["a"])(l,c,a,!1,null,null,null);e["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d2163ab.59bf9907.js.map
--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {},
-    "postcss-px-to-viewport": {
-      viewportWidth: 375, //视图的宽度,对应设计稿的宽度
-      viewportHeight: 667, //视图的高度,对应设计稿的高度
-      unitPecision: 5, //指定px转换为视窗单位值的小数位数(很多时候无法整除)
-
-
+    plugins: {
+        autoprefixer: {},
+        "postcss-px-to-viewport": {
+            viewportWidth: 375, //视图的宽度,对应设计稿的宽度 iphone6 
+            viewportHeight: 667, //视图的高度,对应设计稿的高度
+            unitPecision: 5, //指定px转换为视窗单位值的小数位数(很多时候无法整除)
+            viewportUnit: 'vw', //指定需要转换成的视图单位,建议vw
+            selectorBlackList: ['ignore', 'tab-bar'], //指定不需要转换的类 以tab-bar开头的类
+            minPixelValue: 1, //小于等于1px 不转换成视窗单位
+            mediaQuery: false //允许在媒体查询中转换px
+        }
     }
-  }
 }
